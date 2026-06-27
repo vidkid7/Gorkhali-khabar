ec67562e0cc7915727b3d37eb02aed88d00dc1f3
vidkid7 <vidkid7@users.noreply.github.com>
2026-06-27 10:53:07 +0545

    Allow public media URL previews

--- src/proxy.ts
+++ src/proxy.ts
@@ -145,7 +145,7 @@ export function proxy(request: NextRequest) {
     "default-src 'self'",
     `script-src ${scriptSrc.join(" ")}`,
     "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
-    "img-src 'self' data: blob: https://res.cloudinary.com https://*.googleusercontent.com https://picsum.photos https://fastly.picsum.photos https://*.railway.app",
+    "img-src 'self' data: blob: https:",
     "font-src 'self' https://fonts.gstatic.com",
     "connect-src 'self' https://accounts.google.com https://wttr.in https://*.railway.app",
     "frame-src https://accounts.google.com https://www.youtube.com https://www.youtube-nocookie.com",
