5eb956d7cdcfa19a95ff0bbfbb8aea025bb3767d
officialaccoount00-oss <officiala.ccoount00@gmail.com>
2026-07-24 20:04:16 +0545

    Avoid duplicate Prisma generation during deploy

--- scripts/cpanel-deploy.sh
+++ scripts/cpanel-deploy.sh
@@ -29,8 +29,8 @@ cloudlinux-selector stop --interpreter nodejs --user gorkhal1 --app-root "$APP_R
 set +u
 source "$NODE_ENV_ACTIVATE"
 set -u
+export NODE_OPTIONS="${NODE_OPTIONS:-} --v8-pool-size=1"
 npm ci
-npx prisma generate
 npx next build --webpack
 mkdir -p "$APP_ROOT/tmp"
 touch "$APP_ROOT/tmp/restart.txt"
